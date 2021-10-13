0f75915bb8bd5a1ac3e5dd9571e5f7d2da980607
Asheya Casey Yuson <90026029+asheyacasey@users.noreply.github.com>
2021-10-13 13:24:17 +0800

    Add files via upload

---
+++ AngularCalculatorVer2/src/app/numbers/numbers.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-numbers',
+  templateUrl: './numbers.component.html',
+  styleUrls: ['./numbers.component.css']
+})
+export class NumbersComponent /* implements OnInit */ {
+/*
+  constructor() { }
+  answer = '';
+  aNum = 0;
+  bNum = 0;
+  fNan = false;
+  sNan = false;
+  
+  op = '';
+  empOne = false;
+  empTwo = false;
+  
+ 
+
+  ngOnInit(): void {
+  }
+
+  getfirstNum(num: number) {
+    this.aNum = num;
+  }
+
+  getsecNum(num: number) {
+    this.aNum = num;
+  }
+  
+
+ 
+*/
+  
+
+}
