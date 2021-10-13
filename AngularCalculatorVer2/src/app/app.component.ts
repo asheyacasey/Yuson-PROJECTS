058b8600c1fe8740b87ebbde8a8647aeb6d0f00d
Asheya Casey Yuson <90026029+asheyacasey@users.noreply.github.com>
2021-10-13 13:22:38 +0800

    Add files via upload

---
+++ AngularCalculatorVer2/src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  /*title = 'calculatorVer2';
+  answer = '';
+ 
+  fNUM = 0;
+  sNUM = 0;
+  op = '';
+  empOne = false;
+  empTwo = false;
+
+
+  getInput1(num: number)
+  {
+    this.fNUM = num;
+  }
+
+  getInput2(num: number)
+  {
+    this.sNUM = num;
+  }
+
+  getBoolean1(nan: boolean)
+  {
+    this.fNan = nan;
+  }
+
+  getBoolean2(nan: boolean)
+  {
+    this.sNan = nan;
+  }
+
+  getRes(ans: string)
+  {
+    this.answer = ans;
+  }
+
+  getOperation(choice: string)
+  {
+    this.op = choice;
+  }
+
+  getEmpty1(field: boolean)
+  {
+    this.empOne = field;
+  }
+
+  getEmpty2(field: boolean)
+  {
+    this.empTwo = field;
+  }
+ 
+*/
+  
+}
