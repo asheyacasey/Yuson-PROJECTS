9c867c596df2dfb4d3feba175dbed618ef37d6d0
Asheya Casey Yuson <90026029+asheyacasey@users.noreply.github.com>
2021-10-13 13:25:28 +0800

    Add files via upload

---
+++ AngularCalculatorVer2/src/app/operation/operation.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-operation',
+  templateUrl: './operation.component.html',
+  styleUrls: ['./operation.component.css']
+})
+export class OperationComponent /*implements OnInit*/ {
+/*
+  @Input() emptyF !: boolean;
+  @Input() emptyS !: boolean;
+  @Output() operation = new EventEmitter<string>();
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  getOp (choice: string)
+  {
+    this.operation.emit(choice);
+  }
+  */
+
+}
