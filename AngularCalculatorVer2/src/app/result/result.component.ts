9d52829936dc05ebc86306a8a8693d09ecb60f90
Asheya Casey Yuson <90026029+asheyacasey@users.noreply.github.com>
2021-10-13 13:26:28 +0800

    Add files via upload

---
+++ AngularCalculatorVer2/src/app/result/result.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.css']
+})
+export class ResultComponent /*implements OnInit */ {
+/*
+  answer = 0;
+
+
+  @Input() finalAns !: string;
+  @Input() fNum !: number;
+  @Input() sNum !: number;
+  @Input() choice !: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+ /*
+  toCalculate(choice: string)
+  {
+   
+    let op = parseInt(choice);
+
+    
+      switch(op)
+      {
+        case 1: this.answer = this.fNum + this.sNum; break;
+        case 2: this.answer = this.fNum - this.sNum; break;
+        case 3: this.answer = this.fNum * this.sNum; break;
+        case 4: this.answer = this.fNum / this.sNum; break;
+      }
+      this.finalAns = this.answer.toString();
+
+      if(op === 4 && (this.sNum === 0 && this.fNum !== 0))
+        this.finalAns = "Divisor can not be zero!";
+   
+
+    //this.onClick();
+  
+*/
+    }
